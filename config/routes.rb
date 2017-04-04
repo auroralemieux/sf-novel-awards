79ace85e0130d8786d35d9d5a996a9fef12e9bf0
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-04 10:28:14 -0700

    index books and years, app nav and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,31 @@
 Rails.application.routes.draw do
 
   # resources :years, only: [:index, :show, :new] #for example
-  
+
   root to: 'years#index'
 
   get '/years', to: 'years#index'
-  get '/years/:id', to: 'years#show', as: 'year'
-
   get '/years/new', to: 'years#new'
   post '/years', to: 'years#create'
-
+  get '/years/:id', to: 'years#show', as: 'year'
   get '/years/:id/edit', to: 'years#edit'
   patch '/years/:id', to: 'years#update'
-
   delete '/years/:id', to: 'years#destroy', as: 'delete_year'
 
   get '/authors', to: 'authors#index'
-  get '/authors/:id', to: 'authors#show', as: 'author'
-
   get '/authors/new', to: 'authors#new'
   post '/authors', to: 'authors#create'
-
+  get '/authors/:id', to: 'authors#show', as: 'author'
   get '/authors/:id/edit', to: 'authors#edit'
   patch '/authors/:id', to: 'authors#update'
-
   delete '/authors/:id', to: 'authors#destroy', as: 'delete_author'
 
   get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show', as: 'book'
-
   get '/books/new', to: 'books#new'
   post '/books', to: 'books#create'
-
+  get '/books/:id', to: 'books#show', as: 'book'
   get '/books/:id/edit', to: 'books#edit'
   patch '/books/:id', to: 'books#update'
-
   delete '/books/:id', to: 'books#destroy', as: 'delete_book'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
